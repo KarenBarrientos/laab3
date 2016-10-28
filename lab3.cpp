15cd54a976207a7e81f71d81c7cf434f7c923440
KarenBarrientos <karenp.bmedina@unitec.edu>
2016-10-28 17:10:25 -0600

    lol

--- lab3.cpp
+++ lab3.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 int menu();
-int*** crearMatriz(int);
-void imprimir(int** matrix,int Dividiendo,int grado);
+void impresion(int***,int,int);
 
 int main(int argc, char const *argv[]){
 	int opcion;
-	while ( (opcion = menu()) != 4){
+	char continuar;
+	do{
+		cout<<endl;
+		cout <<"***** MENU ***** "<< endl 
+         << "1. Ejercicio1" << endl
+         << "2. Ejercicio2" << endl
+         << "3. Ejercicio3" << endl
+         << "4. Salir" << endl;
+         cin >> opcion;
 
 		if (opcion == 1){
 			int anio=1901;
@@ -17,7 +25,7 @@ int main(int argc, char const *argv[]){
 			int domingos = 0;
 			int cantidadDias=1;
 			int meses[] = {31,28,31,30,31,30,31,31,30,31,30,31};
- 
+ 			cout<<"-------------- EJERCICIO 1 --------------"<<endl;
 			for(int i=anio; i<=final; i++){
 				for(int j=0;j<12;j++){
 					if(i>1901){
@@ -40,43 +48,50 @@ int main(int argc, char const *argv[]){
 		} // fin if 
 
 		if (opcion==2){
-			int gradoAlto,contador=0;
+			int gradoAlto,contador=0,polinomio;
 			int a;
 			cout<<"-------------- EJERCICIO 2 --------------"<<endl;
 			cout<<"Ingrese el grado mas alto del polinomio: "<<endl;
 			cin>>gradoAlto;
 
-			int** matriz = new int*[3];
-
-			for(int i=0;i<gradoAlto;i++){
-				matriz[i]=new int[gradoAlto];
+			int*** matriz = new int**[gradoAlto+1];
+			for(int i = 0;i<gradoAlto+1;i++){
+				matriz[i] = new int*[3];
+				for(int j = 0;j < 3;j++){
+					matriz[i][j] = new int[gradoAlto+1];
+				}
 			}
-			for(int i=gradoAlto; i>=0; i--){
-				cout<<"Ingrese el polinomio x^"<<i<<": ";
-				cin>> matriz[0][contador];
+			for(int i = gradoAlto;i>=0;i--){
+				cout<<"Ingrese el polinomio de grado x^"<<i<<": ";
+				cin>>polinomio;
+				matriz[0][0][contador] = polinomio;
 				contador++;
 			}
-
-			cout<<"Ingrese a:";
+			cout<<"Ingrese a: ";
 			cin>>a;
-			
-			for(int i=0;i<2;i++){
-				if(i==0){
-					matriz[1][0]=matriz[0][0];
-					matriz[2][0]=matriz[0][0];
-					imprimir(matriz,a,gradoAlto);
-				}else{
-					for(int j=1;j<=gradoAlto;j++){
-						matriz[i][j]=matriz[i+1][j-1];
-						matriz[i+1][j]=matriz[i-1][j]+matriz[i][j];
-						imprimir(matriz,a,gradoAlto);
+
+			for(int i=0;i<gradoAlto;i++){
+				for(int j=0; j<2; j++){
+					if(j == 0){
+						matriz[i][1][0] = matriz[i][0][0];
+						matriz[i][2][0] = matriz[i][0][0];
+					}else{
+						for(int k = 1; k<=gradoAlto; k++){
+							matriz[i][j][k] = matriz[i][j+1][k-1] * a;
+							matriz[i][j+1][k] = matriz[i][j-1][k] + matriz[i][j][k];
+						}
 					}
 				}
 			}
-			cout<<"El Residuo es :"<<matriz[2][gradoAlto]<<"\n";
-
-			//efectuarDivision(matriz, tamanio);
-			//crearMatriz(matriz,gradoAlto);
+			cout<<endl<<"Residuo= "<<matriz[0][2][gradoAlto];
+			impresion(matriz,a,gradoAlto);
+			for(int i = 0;i<gradoAlto;i++){
+				for(int j = 0;j < 3;j++){
+					delete[] matriz[i][j];
+				}
+				delete[] matriz[i];
+			}
+			delete[] matriz;
 
 		}// fin if2
 
@@ -89,56 +104,27 @@ int main(int argc, char const *argv[]){
 
 		}
 
-	}// fin while
+		cout<<"Desea Continuar[s/n]: ";
+		cin>>continuar;
+
+	}while(opcion = menu() != 4 && continuar == 's'||continuar == 'S');
 
 	return 0;
 }
 
-int menu(){
-    cout<<endl;
-    cout <<"***** MENU ***** "<< endl 
-         << "1. Ejercicio1" << endl
-         << "2. Ejercicio2" << endl
-         << "3. Ejercicio3" << endl
-         << "4. Salir" << endl;
-    int opcion;
-    do{
-        cin >> opcion;
-        if (opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4)
-            cerr << "Introduzca un valor correcto" << endl;
-    }while (opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4);
 
-    return opcion;
-}
-
-int*** crearMatriz(int size){
-	int*** matriz=new int**[size];
-	for (int i = 0; i < size; ++i){
-		matriz[i]=new int*[3];
+void impresion(int*** matrix,int a,int grado){
+	for(int i = 0;i< grado+1;i++){
+		cout<<setfill('-')<<setw(20);
+		for(int j = 0;j<3;j++ ){
+			for(int k = 0;k<grado+1;k++){
+				cout<<setfill(' ')<<setw(4)<<matrix[i][j][k];
+			}
+			if(j== 0){
+				cout<<"|"<<a;
+			}
+			cout<<endl;
+		}
+		cout<<endl;
 	}
-	for (int i = 0; i < size; ++i){
-		for (int j = 0; j < 3; ++j){
-			matriz[i][j]= new int[size];
-		}
-	}
-	return matriz;
-
-}
-
-void imprimir(int** matrix,int Dividiendo,int grado){
-	for(int i=0;i<3;i++){
-		for(int j=0;j<grado+1;j++){
-			cout<<" "<<matrix[i][j];
-		}
-		if(i<2){
-			cout<<"| "<<Dividiendo;
-		}
-			
-		cout<<"\n";
-		if(i==2){
-			cout<<"------------------------ \n";
-		}
-	}
-	cout<<"\n";
-	
 }
